788d7c850711266b9c31ffc2ac3b75f658cd83b1
RheatheCoder <87926743+RheatheCoder@users.noreply.github.com>
2022-04-03 17:25:52 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function addUser(){
+    player1= document.getElementById("player1_name_input").value ;
+    player2= document.getElementById("player2_name_input").value ;
+    localStorage.setItem("player1 name", player1);
+    localStorage.setItem("player2 name", player2);
+    window.location = "gamepage.html";
+}
